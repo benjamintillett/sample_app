93f52b4284f7ec880f7b3efbf3d04384e5e05108
ben tillett <benjamin.tillett@gmail.com>
2013-11-20 10:20:27 +0000

    finish static pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,19 +1,20 @@
 require 'spec_helper'
 
-describe StaticPagesController do
+describe "Static pages" do
 
-  describe "GET 'home'" do
-    it "returns http success" do
-      get 'home'
-      response.should be_success
+  describe "Home page" do
+
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_content('Sample App')
     end
   end
 
-  describe "GET 'help'" do
-    it "returns http success" do
-      get 'help'
-      response.should be_success
+  describe "Help page" do
+
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content('Help')
     end
   end
-
 end
