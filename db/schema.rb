3e4c33727959662d758a8c3744310e4231c61e4d
ben tillett <benjamin.tillett@gmail.com>
2013-11-25 06:38:44 +0000

    make basic user model including password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131124112726) do
+ActiveRecord::Schema.define(:version => 20131125053133) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
